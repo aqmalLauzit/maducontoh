74c99e6686a6c09879ffa9a2ed668896284c6a8a
aqmalLauzit <maqmalh2001@gmail.com>
2022-06-03 22:08:49 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container : {
+      center: true,
+      padding : '16px',
+    },
+    extend: {
+      colors : {
+        primary: '#f59e0b',
+        secondary : '#64748b',
+        dark : '#78350f'
+      },
+      screens: {
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
